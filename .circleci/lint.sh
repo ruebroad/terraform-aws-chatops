3aad4a93e5e5ba854964e4686a56303584724683
Rupert <rupert.broad@gmail.com>
2020-12-05 15:26:37 +0100

    fix:

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-lint_failed="no"
-for file in $(find . -name "*tf")
-do
-    lines=$(terraform fmt -write=false -list=true ${file} | wc -l | sed 's/[^0-9]//g')
-    if [ ${lines} != "0" ]
-    then
-    echo "Please run terraform fmt ${file}"
-    lint_failed="yes"
-    fi
-done
-if [ ${lint_failed} != "no" ]; then exit 1; fi
+failures=$(terraform fmt -write=false -list=true -recursive)
+if [ -n $failures ]; then echo -e "Following files need to be formatted\n" $failures; exit 1; fi
--- .circleci/validate.sh
+++ .circleci/validate.sh
@@ -1,9 +1,10 @@
 
 #!/bin/sh
+AWS_DEFAULT_REGION="eu-west-1"
 
+# tflint
 apk add --update openssl
 apk add curl
-# wget https://github.com/wata727/tflint/releases/download/v0.5.4/tflint_linux_amd64.zip
 tflint_latest_url=$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")
 wget $tflint_latest_url
 unzip tflint_linux_amd64.zip
@@ -12,16 +13,5 @@ export PATH=/usr/local/tflint/bin:$PATH
 install tflint /usr/local/tflint/bin
 tflint 
 
-validate_failed="no"
-TF_DIRS=$(find $(pwd) -type f -iname "*.tf*" -exec dirname '{}' \; | grep -v ".terraform" | sort | uniq | xargs echo)
-for DIR in $TF_DIRS
-do
-    echo Processing $DIR
-    lines=$(terraform validate -input=false -check-variables=false -no-color $DIR | wc -l | sed 's/[^0-9]//g')
-    if [ ${lines} != "0" ]
-    then
-    echo "Please run terraform validate ${file}"
-    validate_failed="yes"
-    fi
-done
-if [ ${validate_failed} != "no" ]; then exit 1; fi
+# validate
+find . -name ".terraform" -prune -o -type f -name "*.tf" -exec dirname {} \;|sort -u | while read m; do (cd "$m" && terraform validate) || exit 1 ; done
