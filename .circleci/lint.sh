fad5ebf0c955d599c02455a854c705c022032dee
Rupert <rupert.broad@gmail.com>
2020-12-05 15:54:16 +0100

    fix: shellcheck

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -2,7 +2,7 @@
 failures=$(terraform fmt -write=false -list=true -recursive)
 if [[ -n ${failures} ]]; then 
   printf "Following files need to be formatted\n" 
-  printf "$failures"
+  printf '%s' "$failures"
   terraform fmt -diff -write=false -recursive
   exit 1
 fi
