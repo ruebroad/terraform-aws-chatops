3abbfed72c073a03c5c1222a33c63604445f407f
Rupert <rupert.broad@gmail.com>
2020-12-05 15:53:20 +0100

    fix: lint

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 failures=$(terraform fmt -write=false -list=true -recursive)
-if [ -n $failures ]; then 
-  printf "Following files need to be formatted\n" $failures
+if [[ -n ${failures} ]]; then 
+  printf "Following files need to be formatted\n" 
+  printf "$failures"
   terraform fmt -diff -write=false -recursive
   exit 1
 fi
